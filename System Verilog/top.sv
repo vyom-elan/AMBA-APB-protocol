b774147b23575b7d220030fd17536dd97984926e
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-04-09 22:25:01 +0530

    Update top.sv

--- System Verilog/top.sv
+++ System Verilog/top.sv
@@ -1,15 +1,33 @@
-`include "APB_master.v"
-`include "APB_slave.v"
-module APB_top(input PCLK,
-               input PRESETn,
-               input Transfer,
-               input Wr_Rd,
-               input [4:0] Address,
-               input [31:0] write_data,
-               output [31:0] read_data);
-    wire [4:0] PADDR;
-    wire [31:0] PWDATA,PRDATA;
-    wire PSELx,PENABLE,PWRITE,PREADY,PSLVERR;
-    APB_master Design_master(Transfer,Wr_Rd,Address,write_data,read_data,PCLK,PRESETn,PREADY,PRDATA,PSLVERR,PADDR,PSELx,PENABLE,PWRITE,PWDATA);
-    APB_slave Design_slave(PCLK,PRESETn,PADDR,PSELx,PENABLE,PWRITE,PWDATA,PREADY,PRDATA,PSLVERR);
+`include "Master.sv"
+`include "Slave.sv"
+`timescale 1ns/1ns
+module APB(input PCLK,PRESETn,transfer,READ_WRITE,
+                    input [8:0] apb_write_paddr,
+		            input [7:0]apb_write_data,
+		            input [8:0] apb_read_paddr,
+		            output PSLVERR, 
+                    output [7:0] apb_read_data_out);
+    wire [7:0]PWDATA,PRDATA,PRDATA1,PRDATA2;
+    wire [8:0]PADDR;
+    wire PREADY,PREADY1,PREADY2,PENABLE,PSEL1,PSEL2,PWRITE;
+    assign PREADY = PADDR[8] ? PREADY2 : PREADY1 ;
+    assign PRDATA = READ_WRITE ? (PADDR[8] ? PRDATA2 : PRDATA1) : 8'dx ;
+    master_bridge dut_mas(apb_write_paddr,
+		                  apb_read_paddr,
+		                  apb_write_data,
+		                  PRDATA,         
+	                      PRESETn,
+		                  PCLK,
+		                  READ_WRITE,
+		                  transfer,
+		                  PREADY,
+	                      PSEL1,
+		                  PSEL2,
+		                  PENABLE,
+	                      PADDR,
+	                      PWRITE,
+	                      PWDATA,
+		                  apb_read_data_out,
+		                  PSLVERR); 
+    Slave dut_sl(  PCLK,PRESETn, PSEL1,PENABLE,PWRITE, PADDR[7:0],PWDATA, PRDATA1, PREADY1 );
 endmodule
