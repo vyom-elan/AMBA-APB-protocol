cbb539625b91e942f449197c331627dc91255613
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-04-10 09:50:31 +0530

    Update testbench.sv

--- System Verilog/testbench.sv
+++ System Verilog/testbench.sv
@@ -1,56 +1,68 @@
+`timescale 1ns/1ns
 module APB_tb;
-    reg PCLK, PRESETn, Transfer, Wr_Rd; 
-    reg [4:0] Address; 
-    reg [31:0] write_data; 
-    wire [31:0] read_data; 
-    APB_top DUT (PCLK,PRESETn,Transfer,Wr_Rd,Address,write_data,read_data);
-    
-    initial begin
-        PCLK = 0;
-        forever begin
-            #5 PCLK = ~PCLK;
+    reg PCLK,PRESETn,transfer,READ_WRITE;
+    reg [8:0]apb_write_paddr;
+    reg [7:0]apb_write_data;
+    reg [8:0]apb_read_paddr;
+    wire [7:0]apb_read_data_out;
+    wire PSLVERR;
+    reg [7:0]data,expected;
+    reg [7:0]mem[0:15];
+    APB dut_top(PCLK, PRESETn, transfer, READ_WRITE, apb_write_paddr, apb_write_data, apb_read_paddr,
+		       PSLVERR, apb_read_data_out);
+    integer i,j;
+    initial
+    begin
+        PCLK <= 0;
+        forever #5 PCLK = ~PCLK;
+    end
+    initial
+    begin
+        PRESETn<=0; transfer<=0; READ_WRITE =0;
+        @(posedge PCLK)      PRESETn = 1;                                     //no write address available but request for write operation
+        @(posedge PCLK)      transfer = 1;
+        repeat(2) @(posedge PCLK);
+        @(negedge PCLK)     write;        // write operation                               
+        @(posedge PCLK);    apb_write_paddr = 9'd526;  apb_write_data = 9'd9;
+        repeat(2) @(posedge PCLK);    apb_write_paddr = 9'd22; apb_write_data = 9'd35;
+        repeat(2) @(posedge PCLK);
+        @(posedge PCLK)     READ_WRITE =1; PRESETn<=0; transfer<=0; 
+        @(posedge PCLK)     PRESETn = 1;
+        repeat(3) @(posedge PCLK)     transfer = 1;                             // no read address available but request for read operation
+        repeat(2) @(posedge PCLK)     read;                             //read operation
+        repeat(3) @(posedge PCLK);   apb_read_paddr = 9'd45;                 //data not inserted in write operation but requested for read operation
+        repeat(4) @(posedge PCLK);
+        $finish;
+    end
+    task write;
+    begin
+        transfer =1;
+	    for (i = 0; i < 8; i=i+1) 
+        begin
+	        repeat(2)@(negedge PCLK)
+            begin    
+                data = i;
+	        	apb_write_data = 2*i;
+		        apb_write_paddr =  {1'b0,data};           
+	        end 
+	    end
+    end
+    endtask
+    task read;
+    begin 
+        for (j = 0;  j< 8; j= j+1)
+        begin
+	        repeat(2)@(negedge PCLK)
+            begin  
+	            data = j; 
+	            apb_read_paddr = {1'b0,data};
+            end
         end
     end
-    
-    
-    initial begin
-        PRESETn  = 1;
-        Transfer = 1;
-        
-        //Write Operation 1
-        Address = 5'b10010;
-        #10
-        Wr_Rd      = 1;
-        write_data = 32'hDEADBEEF;
-        #10
-        
-        //Write Operation 2
-        Address    = 5'b10101;
-        Wr_Rd      = 1;
-        write_data = 32'hDABBCAFE;
-        #30
-        write_data = 32'hx;
-        #10
-        Transfer     = 0;
-        #15 Transfer = 1;
-
-        //Read Operation 1
-        Wr_Rd   = 0;
-        Address = 5'b10010;
-        
-        //Read Operation 2
-        #30
-        Address = 5'b10101;
-        
-        #20 Transfer = 0;
-        #20 $finish;
-        
-    end
-    
-    
-    initial begin
-        $dumpfile("APB_wave.vcd");
+    endtask
+    initial
+    begin
+        $dumpfile("apbWaveform.vcd");
         $dumpvars;
     end
-    
-endmodule
+ endmodule
