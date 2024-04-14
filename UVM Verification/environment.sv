0bca016e855be1c6d85f55a907e0ef960da356e7
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-04-14 21:56:59 +0530

    Update environment.sv

--- UVM Verification/environment.sv
+++ UVM Verification/environment.sv
@@ -1,12 +1,20 @@
 class apb_env  extends uvm_env;
+ 
    `uvm_component_utils(apb_env);
+
+   //ENV class will have agent as its sub component
    apb_agent  agt;
    apb_scoreboard scb;
    apb_subscriber apb_subscriber_h;
+  
+   //virtual interface for APB interface
    virtual dut_if  vif;
+
    function new(string name, uvm_component parent);
       super.new(name, parent);
    endfunction
+
+   //Build phase - Construct agent and get virtual interface handle from test  and pass it down to agent
    function void build_phase(uvm_phase phase);
      super.build_phase(phase);
      agt = apb_agent::type_id::create("agt", this);
@@ -17,6 +25,7 @@ class apb_env  extends uvm_env;
      end
      uvm_config_db#(virtual dut_if)::set( this, "agt", "vif", vif);
    endfunction
+  
    function void connect_phase(uvm_phase phase);
      super.connect_phase(phase);
      agt.mon.ap.connect(scb.mon_export);
