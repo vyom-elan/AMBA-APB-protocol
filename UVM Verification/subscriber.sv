615c85961c8f62cbc4b993c07bc79fa250362ab3
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-04-18 23:22:46 +0530

    Update subscriber.sv

--- UVM Verification/subscriber.sv
+++ UVM Verification/subscriber.sv
@@ -1,8 +1,8 @@
-class apb_subscriber extends uvm_subscriber#(apb_transaction);
+class apb_subscriber extends uvm_subscriber#(apb_transaction);  
   `uvm_component_utils(apb_subscriber)
   bit [31:0] addr;
   bit [31:0] data;
-  covergroup cover_bus;
+    covergroup cover_bus;
     coverpoint addr {
       bins a[16] = {[0:255]};
     }
@@ -10,14 +10,18 @@ class apb_subscriber extends uvm_subscriber#(apb_transaction);
       bins d[16] = {[0:255]};
     }
   endgroup
+  
   function new(string name, uvm_component parent);
     super.new(name,parent);
     cover_bus=new;
   endfunction
+  
   function void write(apb_transaction t);
     `uvm_info("APB_SUBSCRIBER", $psprintf("Subscriber received tx %s", t.convert2string()), UVM_NONE);
+   
     addr    = t.addr;
     data    = t.data;
     cover_bus.sample();
   endfunction
+  
 endclass
