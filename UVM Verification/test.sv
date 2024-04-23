1b7cc089e10e4d1ef893f8932999b211764397c9
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-04-23 22:24:29 +0530

    Update test.sv

--- UVM Verification/test.sv
+++ UVM Verification/test.sv
@@ -1,17 +1,21 @@
 class apb_base_test extends uvm_test;
   `uvm_component_utils(apb_base_test);
-  apb_env  env;
+    apb_env  env;
   virtual dut_if vif;
+  
   function new(string name = "apb_base_test", uvm_component parent = null);
     super.new(name, parent);
   endfunction
+
   function void build_phase(uvm_phase phase);
     env = apb_env::type_id::create("env", this);
+    //
     if (!uvm_config_db#(virtual dut_if)::get(this, "", "vif", vif)) begin
       `uvm_fatal("build_phase", "No virtual interface specified for this test instance")
     end 
     uvm_config_db#(virtual dut_if)::set( this, "env", "vif", vif);
   endfunction
+
   task run_phase( uvm_phase phase );
     apb_sequence apb_seq;
     apb_seq = apb_sequence::type_id::create("apb_seq");
@@ -20,5 +24,7 @@ class apb_base_test extends uvm_test;
     apb_seq.start(env.agt.sqr);
     #100ns;
     phase.drop_objection( this , "Finished apb_seq in main phase" );
-  endtask  
+  endtask
+  
+  
 endclass
